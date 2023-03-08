fe8d1a05434674044a4588ca8216f140112d0259
Gulshan K <gulshanrk654@gmail.com>
2023-03-08 22:14:52 +0530

    added three more files

---
+++ Task_1(Basic Python)/6.py
@@ -0,0 +1,16 @@
+'''Question Link:- https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true'''
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0 and year % 100 != 0:
+        leap = True
+    elif year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    
+    return leap
+
+year = int(input('Enter the year:'))
+print(is_leap(year))
